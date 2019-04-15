1a291d50276b98c6dcfe52bff967a7235931445a
Mbugua muthoni <mbuguamuthoni9@gmail.com>
2019-04-15 17:08:20 +0300

    Kia-Ma with firebase

--- app/src/main/java/mbugua/com/kia_ma/MainActivity.java
+++ app/src/main/java/mbugua/com/kia_ma/MainActivity.java
@@ -2,15 +2,21 @@
 package mbugua.com.kia_ma;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,13 +24,26 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
 
+    private Toolbar mainToolbar;
+    private FirebaseAuth mAuth;
 
 
 
-    @Override
+
+
+
+
+
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
+
+
+
+        mainToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mainToolbar);
 
     }
 
@@ -35,17 +54,46 @@ public class MainActivity extends AppCompatActivity {
     FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
         if (currentUser == null){
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
+            sendToLogin();
         }
 
+    }
 
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
 
 
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        switch (item.getItemId()){
+
+            case R.id.action_logout_btn:
+                logOut();
+
+                return true;
+
+                default:
+                    return false;
+
+        }
+
+    }
+
+    private void logOut() {
+        mAuth.signOut();
+        sendToLogin();
+    }
+    private void sendToLogin() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
