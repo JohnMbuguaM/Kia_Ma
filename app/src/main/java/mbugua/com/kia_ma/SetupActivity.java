c71d1e4ac7643cbc35d5fc87760b6427d0e9f05b
Mbugua muthoni <mbuguamuthoni9@gmail.com>
2019-04-17 13:28:03 +0300

    commit saving -->setup  page and getting permission to upload photos

--- app/src/main/java/mbugua/com/kia_ma/SetupActivity.java
+++ app/src/main/java/mbugua/com/kia_ma/SetupActivity.java
@@ -14,16 +14,46 @@ import android.widget.Toast;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import static android.os.Build.VERSION_CODES.M;
+
 public class SetupActivity extends AppCompatActivity {
 
-
     private CircleImageView setupImage;
+
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        Toolbar setupToolbar = findViewById(R.id.setupToolbar);
+        setSupportActionBar(setupToolbar);
+
+
+        setupImage =findViewById(R.id.setup_image);
+        setupImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M) {
+
+                    if (ContextCompat.checkSelfPermission(SetupActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                        Toast.makeText(SetupActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(SetupActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    } else {
+
+                        Toast.makeText(SetupActivity.this, "You Already have permission", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        });
+
 
 
     }
